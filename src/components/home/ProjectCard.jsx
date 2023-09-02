1a6d33b4ab781047372772a6aacda1e79ccc519b
nengelmann <mail@nico-engelmann.de>
2023-09-02 15:06:05 +0200

    adapt  project card layout

--- src/components/home/ProjectCard.jsx
+++ src/components/home/ProjectCard.jsx
@@ -40,13 +40,7 @@ const ProjectCard = ({ value }) => {
 const CardButtons = ({ svn_url }) => {
   return (
     <div className="d-grid gap-2 d-md-block">
-      <a
-        href={`${svn_url}/archive/master.zip`}
-        className="btn btn-outline-secondary mx-2"
-      >
-        <i className="fab fa-github" /> Clone Project
-      </a>
-      <a href={svn_url} target=" _blank" className="btn btn-outline-secondary mx-2">
+      <a href={svn_url} target=" _blank" className="btn btn-outline-secondary mx-0">
         <i className="fab fa-github" /> Repo
       </a>
     </div>
