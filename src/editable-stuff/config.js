e2c8d31746bf8b2fa6e84125a7ec4e45943ee415
nengelmann <mail@nico-engelmann.de>
2023-09-18 16:48:00 +0200

    add MeetingCam to projects

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -57,7 +57,7 @@ const repos = {
   heading: "Recent Projects",
   gitHubUsername: "nengelmann", //i.e."johnDoe12Gh"
   reposLength: 0,
-  specificRepos: ["custom-dataset.com","M3T", "home"],
+  specificRepos: ["MeetingCam", "custom-dataset.com", "M3T", "home"],
 };
 
 // Leadership SECTION
